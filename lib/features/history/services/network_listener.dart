6e1e7a93838c65730824da3d52c4b34df4c55db4
Kim Hsiao <white.shopping@gmail.com>
2026-05-25 22:00:28 +0800

    feat: add NetworkRestored callback to NetworkListener

--- lib/features/history/services/network_listener.dart
+++ lib/features/history/services/network_listener.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
 
 typedef NetworkCallback = Future<void> Function();
+typedef NetworkRestoredCallback = void Function();
 
 class NetworkListener {
   final Connectivity _connectivity;
   StreamSubscription<List<ConnectivityResult>>? _subscription;
   NetworkCallback? onRestored;
+  NetworkRestoredCallback? _onNetworkRestored;
   bool _wasOffline = false;
 
   NetworkListener({
@@ -16,12 +18,21 @@ class NetworkListener {
     _startListening();
   }
 
+  void setOnNetworkRestored(NetworkRestoredCallback? callback) {
+    _onNetworkRestored = callback;
+  }
+
+  void _notifyNetworkRestored() {
+    _onNetworkRestored?.call();
+  }
+
   void _startListening() {
     _subscription = _connectivity.onConnectivityChanged.listen((results) async {
       final hasConnection = results.any((r) => r != ConnectivityResult.none);
 
       if (hasConnection && _wasOffline) {
         await onRestored?.call();
+        _notifyNetworkRestored();
       }
 
       _wasOffline = !hasConnection;
